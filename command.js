6c282863fc62d82a363c0d4f3075ce5cba93532c
kmw070806 <63104149+kmw070806@users.noreply.github.com>
2020-04-08 13:34:19 +0900

    Create command.js

---
+++ command.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+ 
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+// Set the prefix
+client.on("message", (message) => {
+
+  if (message.author.bot) return;
+
+  if (message.content.startsWith("안녕")) {
+    message.channel.send(`${`<@${message.author.id}> `}님 안녕하세요.`)
+  } 
+});
